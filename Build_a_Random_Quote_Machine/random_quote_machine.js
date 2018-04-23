d3bb6e36f99c8573c9e42de1f639825a4921ff95
Krittiya Clark <krittiyaclark@gmail.com>
2018-04-22 22:27:31 -0400

    Working quote app

---
+++ Build_a_Random_Quote_Machine/random_quote_machine.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	function randomQuote() {
+		var display = document.querySelector('#new-quote');
+
+		$.ajax({
+		    url: "https://api.forismatic.com/api/1.0/?",
+		    dataType: "jsonp",
+		    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+		    success: function(json) {
+		    	$('#quotation').html(('<i class="fas fa-quote-left"></i> ' + json.quoteText + ' <i class="fas fa-quote-right"></i>'));
+				$('#author').html(('-- ' + json.quoteAuthor + ' --'));
+				$('#tweet-quote').attr('href', 'https://twitter.com/home/?status=' + json.quoteText + '-- ' + json.quoteAuthor + ' --');
+	     		},
+	  });
+	}
+
+	$(function() {
+		randomQuote();
+	});
+
+	$('#new-quote').on('click', function(){
+		randomQuote()
+	});
+});
+
